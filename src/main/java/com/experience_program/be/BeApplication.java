0bb75a8d5ee68d6766df40bb0dc86f9ac2aaddbc
Amilaje <choijinho321@gmail.com>
2025-11-07 12:39:29 +0900

    be:first commit main

---
+++ src/main/java/com/experience_program/be/BeApplication.java
@@ -0,0 +1,13 @@
+package com.experience_program.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeApplication.class, args);
+	}
+
+}
